a72888e15050b66673fea91f50d620c37d5c6a53
boudjelaba <56698791+boudjelaba@users.noreply.github.com>
2023-12-22 09:36:36 +0100

    Add files via upload

---
+++ Inscription/index.php
@@ -0,0 +1,22 @@
+﻿<?php
+	// Initialiser la session
+	session_start();
+	// Vérifier si l'utilisateur est connecté, sinon redirection vers la page de connexion
+	if(!isset($_SESSION["nom"])){
+		header("Location: login.php");
+		exit(); 
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+		<div class="sucess">
+		<h1>Bienvenue <?php echo $_SESSION['nom']; ?>!</h1>
+		<p>C'est votre tableau de bord.</p>
+		<a href="logout.php">Déconnexion</a>
+		</div>
+	</body>
+</html>
