058543660c7af96b816dea51c9dd4bc0cb6de96c
boudjelaba <56698791+boudjelaba@users.noreply.github.com>
2024-03-08 07:50:29 +0100

    Delete Exp2 directory

--- Exp2/cookie_init.php
+++
@@ -1,7 +0,0 @@
-<?php
-	setcookie("panier[Portable]", 0);
-	setcookie("panier[Chargeur]", 0);
-	setcookie("panier[Disque]", 0);
-	setcookie("panier[Câble]", 0);
-	header("Location: cookie_ajout.php");
-?>
